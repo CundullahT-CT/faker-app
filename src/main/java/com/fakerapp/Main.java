92f24267d34ac0d130827ef295fffabecc4486ed
CundullahT-CT <cterzioglu@cybertekschool.com>
2025-01-20 18:36:48 +0100

    initial commit

---
+++ src/main/java/com/fakerapp/Main.java
@@ -0,0 +1,15 @@
+package com.fakerapp;
+
+import com.github.javafaker.Faker;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Faker faker = new Faker();
+
+        System.out.println(faker.lordOfTheRings().character());
+
+    }
+
+}
